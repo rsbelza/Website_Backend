f5bb1e96b3d15c434c842b0d21f72c883d789e30
rsbelza <rsbelza@gmail.com>
2024-04-05 18:35:31 +0800

    added products name in order

--- models/Order.js
+++ models/Order.js
@@ -4,6 +4,7 @@ const orderSchema = new mongoose.Schema({
 	userId: {type:String,required:true},
 	productsOrdered:[{
 		productId: {type:String,required: true},
+		productName: {type:String,required: true},
 		quantity: {type:Number,required: true},
 		subtotal: {type:Number,required: true}
 	}],
