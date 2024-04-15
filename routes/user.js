ab4613e59ebe8a94e17f7bb42979c1055d56de53
rsbelza <rsbelza@gmail.com>
2024-04-15 09:58:56 +0800

    fix

--- routes/user.js
+++ routes/user.js
@@ -21,49 +21,4 @@ router.get('/details', verify, userController.retrieveUser)
 router.patch('/update-password', verify, userController.updatePassword)
 
 
-
-
-
-// Blank ko muna yung sa mga google
-/*router.get('/google', 
-	passport.authenticate('google', {
-		scope:['email', 'profile'],
-	}
-));
-
-router.get('/google/callback', 
-	passport.authenticate('google', {
-		failureRedirect: '/user/failed',
-	}),
-	function (req, res) {
-		res.redirect('/user/success')
-	}
-);
-
-router.get("/failed", (req, res) => {
-	console.log('User is not authenticated')
-	res.send("Failed")
-})
-
-router.get("/success", isLoggedIn, (req, res) => {
-	console.log('You are logged in')
-	console.log(req.user)
-	res.send(`Welcome ${req.user.displayName}`)
-})
-
-router.get("/logout", (req, res) => {
-	req.session.destroy((err) => {
-		if (err) {
-			console.log('Error while destroying session:', err)
-		} else {
-			req.logout(() => {
-				console.log('You are logged out');
-				res.redirect('/');
-			})
-		}
-	})
-})*/
-
-
-
 module.exports = router;
